7c88828d9a3a825286f488e736a7ab5ae4a7d1e8
Stéphane ANCELOT <sancelot@numalliance.com>
2019-06-20 15:55:11 +0200

    each thread is linked to a CPU core

--- thread_pool.cpp
+++ thread_pool.cpp
@@ -41,14 +41,19 @@ for (int n=0;n < num_threads;n++)
 {
     pthread_t *t = new pthread_t;
     ret = pthread_create(t,NULL,&job,NULL);
+    // each allocated thread is linked to a core.
+    cpu_set_t cpuset;
+	CPU_ZERO(&cpuset);
+	CPU_SET(1 << n,&cpuset);
+    pthread_setaffinity_np(*t,sizeof(cpu_set_t), &cpuset);
+
     threads.push_back(t);
 }
-for (int n=0;n < 100000;n++)
-while (1==1)
-{
-    PushJob();    PushJob();
-
-}
+// testing purpose infinite job loop
+    while (1==1)
+    {
+    PushJob();  
+    }
 }
 
 // push a job in the job queue
@@ -57,10 +62,20 @@ void ThreadPool::PushJob()
     sem_post(&jobs);
 }
 
-
+ino_t getNbCores()
+{
+#ifdef WIN32
+    SYSTEM_INFO sysinfo;
+    GetSystemInfo(&sysinfo);
+    return sysinfo.dwNumberOfProcessors;
+#else
+    return (int) sysconf(_SC_NPROCESSORS_ONLN);
+#endif
+}
 int main()
 {
+printf("core availables : %d\n",getNbCores());
 mlockall(MCL_CURRENT|MCL_FUTURE);
-ThreadPool *pool = new ThreadPool(4);
+ThreadPool *pool = new ThreadPool(getNbCores());
 pause();
 }

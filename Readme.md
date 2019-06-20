d58d3f61312072831c772043a3fa051494c60a51
Stéphane ANCELOT <sancelot@numalliance.com>
2019-06-20 15:35:50 +0200

    thread pool v1

---
+++ Readme.md
@@ -0,0 +1,10 @@
+ThreadPool :
+Creating threads is very consuming memory and kernel accesses must be limited for performance
+so a thread pool is setted up
+
+Bottlenecks :
+File reading and writing are slow operations
+
+disk swap:
+for efficiency , swap of the application is forbidden.
+It implies that machine with not enough memory will not being able use this program.
---
+++ thread_pool.cpp
@@ -0,0 +1,64 @@
+#include <vector>
+#include <pthread.h>
+#include <semaphore.h> 
+#include <stdio.h>
+#include <unistd.h>
+#include <math.h>
+static sem_t   jobs;    /* number of job requested   */
+
+class ThreadPool {
+private:
+    std::vector <pthread_t *> threads;
+public:
+    ThreadPool(int num_threads);
+    void PushJob();
+    static void *job(void *arg);
+}; 
+
+void *ThreadPool::job(void *arg)
+{
+    int ret;
+    while (1 == 1)
+    {
+    ret = sem_wait(&jobs);
+    static float alpha = 0.000001;
+    alpha += 0.00000001;
+    if (alpha > 1.)
+       alpha = 0.;
+    float x = cos(alpha);
+    //printf("job is running\n");
+    }
+}
+
+ThreadPool::ThreadPool(int num_threads)
+{
+int ret;
+
+ret = sem_init(&jobs, 0, 0);
+
+for (int n=0;n < num_threads;n++)
+{
+    pthread_t *t = new pthread_t;
+    ret = pthread_create(t,NULL,&job,NULL);
+    threads.push_back(t);
+}
+for (int n=0;n < 100000;n++)
+while (1==1)
+{
+    PushJob();    PushJob();
+
+}
+}
+
+// push a job in the job queue
+void ThreadPool::PushJob()
+{
+    sem_post(&jobs);
+}
+
+
+int main()
+{
+ThreadPool *pool = new ThreadPool(4);
+pause();
+}

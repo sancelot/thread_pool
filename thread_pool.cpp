8fb154c1b32f088bc96fd8f0306becb3628f3073
Stéphane ANCELOT <sancelot@numalliance.com>
2019-06-20 17:19:00 +0200

    threads are using sched_fifo policy

--- thread_pool.cpp
+++ thread_pool.cpp
@@ -23,10 +23,14 @@ void *ThreadPool::job(void *arg)
     {
     ret = sem_wait(&jobs);
     static float alpha = 0.000001;
+    // burning cycles
+    for (int n=0;n < 10000;n++)
+    {
     alpha += 0.00000001;
     if (alpha > 1.)
        alpha = 0.;
     float x = cos(alpha);
+    }
     //printf("job is running\n");
     }
 }
@@ -39,13 +43,29 @@ ret = sem_init(&jobs, 0, 0);
 
 for (int n=0;n < num_threads;n++)
 {
+    pthread_attr_t tattr;
+
     pthread_t *t = new pthread_t;
-    ret = pthread_create(t,NULL,&job,NULL);
+
+    
+	/* Initilisation des attribut du thread */
+    ret = pthread_attr_init (&tattr); // Attribut par defaut
+    ret = pthread_create(t,&tattr,&job,NULL);
+	struct sched_param param;
+
+
+	ret = pthread_attr_getschedparam (&tattr, &param); // On recupere les attribut par defaut
+	/* Modification attribut apres la creation de la tache */
+	param.sched_priority = 99; // modification de la priorite
+	ret = pthread_setschedparam(* t,SCHED_FIFO, &param); // On applique les different parametre
+    if (ret == -1)
+        perror("pthread_setschedparam failed\n");
+
     // each allocated thread is linked to a core.
     cpu_set_t cpuset;
 	CPU_ZERO(&cpuset);
 	CPU_SET(1 << n,&cpuset);
-    pthread_setaffinity_np(*t,sizeof(cpu_set_t), &cpuset);
+    ret = pthread_setaffinity_np(*t,sizeof(cpu_set_t), &cpuset);
 
     threads.push_back(t);
 }
